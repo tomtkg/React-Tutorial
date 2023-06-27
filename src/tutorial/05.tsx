372e60cb2d22fe0ad8256d002f14585c95a83578
tomtkg <tom.aquamarine@gmail.com>
2023-06-28 00:22:02 +0900

    Create 05.tsx

---
+++ src/tutorial/05.tsx
@@ -0,0 +1,27 @@
+// Typescript does not allow 'Square({ value })'.
+// Question mark allow undefined value.
+function Square({ value }: { value?: string }) {
+  return <button className="square">value</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
