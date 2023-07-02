9d40e4747fcb4b107b1555d9ff308e490696bf0d
tomtkg <tom.aquamarine@gmail.com>
2023-07-03 00:04:14 +0900

    Create 12.tsx

---
+++ src/tutorial/12.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+type Props = {
+  value: string,
+  onSquareClick: any,
+}
+
+function Square({ value, onSquareClick }: Props) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  // Typescript often uses 'useState<T>'.
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
+
+  // Typescript does not allow 'handleClick(i)'.
+  function handleClick(i: number) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
