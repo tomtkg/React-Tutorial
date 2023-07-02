fa6d7ce98a232a61bb927ff555e85bccadb0b8f4
tomtkg <tom.aquamarine@gmail.com>
2023-07-03 00:03:33 +0900

    Create 11.tsx

---
+++ src/tutorial/11.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+// Typescript often uses Props.
+type Props = {
+  value: string,
+  onSquareClick?: React.MouseEventHandler<HTMLButtonElement>,
+  // Also 'onSquareClick?: any' is OK.
+}
+
+function Square({ value, onSquareClick }: Props) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
+}
