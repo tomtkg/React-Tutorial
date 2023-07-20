1109ad420c8234e92ad6514ff2f8c99f0f327fb9
tomtkg <tom.aquamarine@gmail.com>
2023-07-20 20:28:03 +0900

    Update Top.tsx

--- src/base/Top.tsx
+++ src/base/Top.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 
 const file = "https://raw.githubusercontent.com/tomtkg/React-Tutorial/main/README.md"
 
-export default function () {
+export default function Top () {
   const [markdown, setMarkdown] = useState("");
 
   useEffect(() => {
